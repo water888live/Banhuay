d124fb3c94731375ec98dbe8986067923b44c3bc
water888live <water888live@gmail.com>
2019-04-13 17:16:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -156,6 +156,12 @@ app.get('/BanHuayVIP9', function (req, res) {
 	return res.sendStatus(200);
 });
 
+app.get('/FunmoneyPingPong', function (req, res) {
+    const respText = { type: 'text', text: req.query.respText };
+    client.pushMessage('Cdc1a9062ca476a619b83a1f2b597b06e', respText);
+	return res.sendStatus(200);
+});
+
 
 //End of module
 

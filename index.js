c06037507fa8e426aa86c6578d654ec1ffd6063d
water888live <water888live@gmail.com>
2019-04-15 10:54:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get('/BanHuayPingpongRubSub', function (req, res) {
 
 app.get('/PPgummatan', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
-    client.pushMessage('C31ef3d081823bd1ca517cee51a333db3', respText);
+    client.pushMessage('C489301cf8fb99c8b8bd44e68c1f28eb5', respText);
 	return res.sendStatus(200);
 });
 

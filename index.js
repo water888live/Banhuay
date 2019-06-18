c859c07d1daabbb0d4135b08b47b1aab7bc9f100
water888live <water888live@gmail.com>
2019-06-18 09:50:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,20 @@ app.post('/testbot', function(req, res){
     client.pushMessage('Caebad1cc1be04abc1adc6abf521cbb5b', respText);
 	return res.sendStatus(200);
 });
+//============================
+//Line push notification module
+app.get('/lonelyfriend', function (req, res) {
+    const respText = { type: 'text', text: req.query.respText };
+    client.pushMessage('C5bb83eb2a5f690bceea200f7d8825294', respText);
+	return res.sendStatus(200);
+});
 
+app.post('/lonelyfriend', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C5bb83eb2a5f690bceea200f7d8825294', respText);
+	return res.sendStatus(200);
+});
+//============================
 
 app.get('/BanHuay', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
@@ -62,11 +75,25 @@ app.get('/BanHuay', function (req, res) {
 	return res.sendStatus(200);
 });
 
+app.post('BanHuay', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Cfbdafcc3a324d494c241895cc2186def', respText);
+	return res.sendStatus(200);
+});
+//============================
+
 app.get('/BanHuayVIP1', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('C71e61173ab8fd061779aa8a7c45b32f1', respText);
 	return res.sendStatus(200);
 });
+
+app.post('/BanHuayVIP1', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C71e61173ab8fd061779aa8a7c45b32f1', respText);
+	return res.sendStatus(200);
+});
+//============================
 
 app.get('/BanHuayVIP2', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
@@ -74,6 +101,12 @@ app.get('/BanHuayVIP2', function (req, res) {
 	return res.sendStatus(200);
 });
 
+app.post('/BanHuayVIP2', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C58b5f7b1f101b2228de143ea23056567', respText);
+	return res.sendStatus(200);
+});
+//============================
 
 app.get('/BanHuaySuperVIP', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
@@ -81,70 +114,132 @@ app.get('/BanHuaySuperVIP', function (req, res) {
 	return res.sendStatus(200);
 });
 
+app.post('/BanHuaySuperVIP', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Cb625908a1165c7e09274a75339c496d6', respText);
+	return res.sendStatus(200);
+});
+//=============================
+
 app.get('/BanHuayTestRoom5Day', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('Ccbd0377cd442f344e148303835a8cb8b', respText);
 	return res.sendStatus(200);
 });
-
+app.post('/BanHuayTestRoom5Day', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Ccbd0377cd442f344e148303835a8cb8b', respText);
+	return res.sendStatus(200);
+});
+//==============================
 
 app.get('/BanHuayRCAPingpong', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('Cff4272e5b5c57660a255bab2d836a818', respText);
 	return res.sendStatus(200);
 });
+app.post('/BanHuayRCAPingpong', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Cff4272e5b5c57660a255bab2d836a818', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/BanHuayRoumSien', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('C6a98dc021759f053b05c9568beb783f2', respText);
 	return res.sendStatus(200);
 });
-
+app.post('/BanHuayRoumSien', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C6a98dc021759f053b05c9568beb783f2', respText);
+	return res.sendStatus(200);
+});
+//================================
 
 app.get('/BanHuayPingpongRubSub', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('C45d056994156e5261d9bf9c49dc0a6c9', respText);
 	return res.sendStatus(200);
 });
+app.post('/BanHuayPingpongRubSub', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C45d056994156e5261d9bf9c49dc0a6c9', respText);
+	return res.sendStatus(200);
+});
+//================================
 
 app.get('/PPgummatan', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('C489301cf8fb99c8b8bd44e68c1f28eb5', respText);
 	return res.sendStatus(200);
 });
-
-
+app.post('/PPgummatan', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C489301cf8fb99c8b8bd44e68c1f28eb5', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/BanHuayVIP4', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('Cb32438771ec4e1a003c8edf1eb0c2e29', respText);
 	return res.sendStatus(200);
 });
-
+app.post('/BanHuayVIP4', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Cb32438771ec4e1a003c8edf1eb0c2e29', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/BanHuayVIPLemon', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('Cf19817d7d024c87657c480eadd4d049c', respText);
 	return res.sendStatus(200);
 });
+app.post('/BanHuayVIPLemon', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Cf19817d7d024c87657c480eadd4d049c', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/BanHuayTest5Day', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('Ccbd0377cd442f344e148303835a8cb8b', respText);
 	return res.sendStatus(200);
 });
+app.post('/BanHuayTest5Day', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Ccbd0377cd442f344e148303835a8cb8b', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/BanHuayVIP5', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('Cda886585ef55a2cace236c63295e8f2c', respText);
 	return res.sendStatus(200);
 });
+app.post('/BanHuayVIP5', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Cda886585ef55a2cace236c63295e8f2c', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/BanHuayVIP8', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('C2e0db166bc9610345862091bafe079c2', respText);
 	return res.sendStatus(200);
 });
+app.post('/BanHuayVIP8', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C2e0db166bc9610345862091bafe079c2', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 
 app.get('/BanHuayVIP7', function (req, res) {
@@ -152,12 +247,24 @@ app.get('/BanHuayVIP7', function (req, res) {
     client.pushMessage('C63659589145c8825b46b8bfa2f970f2a', respText);
 	return res.sendStatus(200);
 });
+app.post('/BanHuayVIP7', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C63659589145c8825b46b8bfa2f970f2a', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/BanHuayVIP9', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('Ca1f296abf836482c2d16b5ebbb137ab8', respText);
 	return res.sendStatus(200);
 });
+app.post('/BanHuayVIP9', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Ca1f296abf836482c2d16b5ebbb137ab8', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/FunmoneyPingPong', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
@@ -165,18 +272,36 @@ app.get('/FunmoneyPingPong', function (req, res) {
 	console.log(req);
 	return res.sendStatus(200);
 });
+app.post('/FunmoneyPingPong', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('Cdc1a9062ca476a619b83a1f2b597b06e', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/RichPingPong', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('C5a60b1f9d1fd56281fc6c0d9f787d834', respText);
 	return res.sendStatus(200);
 });
+app.post('/RichPingPong', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C5a60b1f9d1fd56281fc6c0d9f787d834', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 app.get('/BanHuayVIP5PP', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
     client.pushMessage('C4c5eb1aa6da1f01b4284796835f64bc9', respText);
 	return res.sendStatus(200);
 });
+app.post('/BanHuayVIP5PP', function(req, res){
+    const respText = { type: 'text', text: req.body.message };
+    client.pushMessage('C4c5eb1aa6da1f01b4284796835f64bc9', respText);
+	return res.sendStatus(200);
+});
+//===============================
 
 
 //End of modul
